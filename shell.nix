c8e8c971c85f30a3d5c36378b8c39917d81b6861
Jeremy Stucki <dev@jeremystucki.ch>
2022-01-24 17:48:41 +0100

    Add Build workflow

--- shell.nix
+++ shell.nix
@@ -1,4 +1,6 @@
-with import <nixpkgs> {};
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/48a50c96e36c3f47e8667998923993f6ba1602db.tar.gz") {}}:
+
+with pkgs;
 let
   pythonEnv = python3.withPackages (ps: [
     ps.requests
